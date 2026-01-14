784d543979541429406f83ce1f18ad9769cea642
faiq57504 <faiq57504@gmail.com>
2026-01-14 10:44:14 +0500

    Time: 3 ms (59.39%) | Memory: 47.1 MB (53.66%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        // LeetCode ke according yahan kabhi nahi aayega
+        return new int[] {};
+    }
+}
